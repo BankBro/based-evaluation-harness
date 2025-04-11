adc5da20b3737c6827fbe10104c26751299fd55f
BankBro <1025052251@qq.com>
2025-04-11 04:57:36 +0000

    refactor(run.sh): 更新模型检查点路径
    - 将模型检查点路径从 '/path/to/checkpoint' 修改为具体的 '/home/lyj/project/based/checkpoints/04-09-based-vq-360m'
    - 此修改使得 run.sh 脚本可以直接运行，无需用户手动指定检查点路径

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export HYDRA_FULL_ERROR=1
 
 lm_eval \
     --model based_lm \
-    --model_args checkpoint=/path/to/checkpoint \
+    --model_args checkpoint=/home/lyj/project/based/checkpoints/04-09-based-vq-360m \
     --tasks swde,fda,squad_completion \
     --device: cuda:0 \
     --batch_size 16
